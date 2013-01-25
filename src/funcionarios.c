f22698ea9459b8e3e4baec9a2855e8b80df01b08
André Walker <andre@andrewalker.net>
2013-01-24 22:45:53 -0200

    Implementar inserir no arquivo

--- src/funcionarios.c
+++ src/funcionarios.c
@@ -29,52 +29,84 @@ int TAMANHO_REGISTRO;
 int REGISTROS_POR_BLOCO;
 int TAMANHO_VETOR_BLOCO;
 int TAMANHO_LASTRO;
+int TAMANHO_BLOCO_DISCO;
 
-void pesquisar(int arquivo, const char *codigo) {
-    lseek(arquivo, 0, SEEK_SET);
+void novo_bloco(struct bloco_funcionarios *b, int c, struct funcionario *f, int *posicao) {
+    b->contador = c;
+    b->vetor    = (struct funcionario *)malloc(TAMANHO_VETOR_BLOCO);
+    b->lastro   = (char *)malloc(TAMANHO_LASTRO);
+
+    int i;
+    for (i = 0; i < c; i++) {
+        b->vetor[i] = f[*posicao];
+        *posicao = *posicao + 1;
+    }
 }
 
 void remover(int arquivo, const char *codigo) {
     lseek(arquivo, 0, SEEK_SET);
 }
 
-void inserir(int arquivo) {
-    lseek(arquivo, 0, SEEK_SET);
+int inserir(int arquivo, int *quantos_blocos, struct bloco_funcionarios **b, struct funcionario f) {
+    if ((*b)[*quantos_blocos - 1].contador == REGISTROS_POR_BLOCO) {
+        *quantos_blocos = *quantos_blocos+1;
+
+        struct bloco_funcionarios *blocos;
+        blocos = (struct bloco_funcionarios *)malloc((*quantos_blocos)*sizeof(struct bloco_funcionarios));
+
+        int i = 0;
+        for (i = 0; i < *quantos_blocos-1; i++) {
+            blocos[i] = (*b)[i];
+        }
+
+        int posicao = 0;
+        novo_bloco(&blocos[ *quantos_blocos-1 ], 1, &f, &posicao);
+
+        *b = blocos;
+    }
+    else {
+        int ultimo_bloco = *quantos_blocos-1;
+        int ultima_posicao_do_bloco = (*b)[ultimo_bloco].contador;
+
+        (*b)[ultimo_bloco].vetor[ ultima_posicao_do_bloco ] = f;
+        (*b)[ultimo_bloco].contador = ultima_posicao_do_bloco+1;
+    }
+
+    lseek(arquivo, TAMANHO_BLOCO_DISCO*(*quantos_blocos-1), SEEK_SET);
+    return escrever_bloco_em_arquivo(arquivo, (*b)[*quantos_blocos-1]);
 }
 
-int criar_arquivo(int quantos_blocos, struct bloco_funcionarios *b) {
-    int arquivo = open("data/arquivo.txt", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-
-    if (arquivo < 0) {
-        printf("Impossível abrir arquivo.\n");
-        return 1;
+int escrever_bloco_em_arquivo(int arquivo, struct bloco_funcionarios b) {
+    if (write(arquivo, &b.contador, sizeof(int)) < 0) {
+        printf("Erro na escrita.\n");
+        return 0;
     }
 
+    int j;
+    for (j = 0; j < b.contador; j++) {
+        if (write(arquivo, &b.vetor[j], TAMANHO_REGISTRO) < 0) {
+            printf("Erro na escrita.\n");
+            return 0;
+        }
+    }
+    if (write(arquivo, &b.lastro, TAMANHO_LASTRO) < 0) {
+        printf("Erro na escrita.\n");
+        return 0;
+    }
+
+    return 1;
+}
+
+int criar_arquivo(int arquivo, int quantos_blocos, struct bloco_funcionarios *b) {
     int i;
-    for (i = 0; i < quantos_blocos; i++) {
-        if (write(arquivo, &b[i].contador, sizeof(int)) < 0) {
-            printf("Erro na escrita.\n");
-            return 1;
-        }
 
-        int j;
-        for (j = 0; j < b[i].contador; j++) {
-            if (write(arquivo, &b[i].vetor[j], TAMANHO_REGISTRO) < 0) {
-                printf("Erro na escrita.\n");
-                return 1;
-            }
-        }
-        if (write(arquivo, &b[i].lastro, TAMANHO_LASTRO) < 0) {
-            printf("Erro na escrita.\n");
-            return 1;
+    for (i = 0; i < quantos_blocos; i++) {
+        if (!escrever_bloco_em_arquivo(arquivo, b[i])) {
+            return 0;
         }
     }
 
-    printf("\n");
-
-    close(arquivo);
-
-    return 0;
+    return 1;
 }
 
 void inicializar_variaveis(const char * caminho_programa) {
@@ -88,25 +120,13 @@ void inicializar_variaveis(const char * caminho_programa) {
     REGISTROS_POR_BLOCO = (int) bloco_menos_contador / TAMANHO_REGISTRO;
     TAMANHO_LASTRO      = bloco_menos_contador % TAMANHO_REGISTRO;
     TAMANHO_VETOR_BLOCO = REGISTROS_POR_BLOCO * TAMANHO_REGISTRO;
+    TAMANHO_BLOCO_DISCO = s.st_blksize;
 
     printf("Tamanho do registro: %5d\n", TAMANHO_REGISTRO);
     printf("Registros por bloco: %5d\n", REGISTROS_POR_BLOCO);
     printf("Tamanho do lastro:   %5d\n", TAMANHO_LASTRO);
 }
 
-
-void novo_bloco(struct bloco_funcionarios *b, int c, struct funcionario *f, int *posicao) {
-    printf("%d, %d\n", c, *posicao);
-    b->contador = c;
-    b->vetor    = (struct funcionario *)malloc(TAMANHO_VETOR_BLOCO);
-    b->lastro   = (char *)malloc(TAMANHO_LASTRO);
-
-    int i;
-    for (i = 0; i < c; i++) {
-        b->vetor[i] = f[*posicao];
-        *posicao = *posicao + 1;
-    }
-}
 
 struct bloco_funcionarios * separar_em_blocos(int quantos_registros, struct funcionario *f, int *qtd_blocos) {
     int quantos_blocos = (int) quantos_registros / REGISTROS_POR_BLOCO;
@@ -157,5 +177,24 @@ int main(int argc, char *argv[]) {
     int quantos_blocos;
     blocos = separar_em_blocos(quantos_registros, f, &quantos_blocos);
 
-    return criar_arquivo(quantos_blocos, blocos);
+    int arquivo = open("data/arquivo.txt", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+    if (arquivo < 0) {
+        printf("Impossível abrir arquivo.\n");
+        return 0;
+    }
+
+    criar_arquivo(arquivo, quantos_blocos, blocos);
+
+    struct funcionario f2;
+    sprintf(f2.codigo, "abc%2d", 200);
+    sprintf(f2.nome, "Funcionário %2d", 200);
+    for (i = 0; i < 9; i++) {
+        f2.data_admissao[i] = data[i];
+    }
+    f2.salario = 2000.00;
+
+    inserir(arquivo, &quantos_blocos, &blocos, f2);
+
+    close(arquivo);
 }
